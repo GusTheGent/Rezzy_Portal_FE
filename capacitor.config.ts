8f467d3726c5aa65864cc14c7cffaf8c5e3bb6b4
angelisk_dev <gusangelis90@gmail.com>
2026-05-15 15:49:25 +0300

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'Rezzy_Portal_FE',
+  webDir: 'www'
+};
+
+export default config;
